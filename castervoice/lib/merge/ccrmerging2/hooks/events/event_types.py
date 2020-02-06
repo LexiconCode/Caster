97ccec59656260408c49574112c4857965ab094b
LexiconCode <thequietplace@protonmail.com>
2020-02-05 22:03:29 -0600

    event_types typo fix

--- castervoice/lib/merge/ccrmerging2/hooks/events/event_types.py
+++ castervoice/lib/merge/ccrmerging2/hooks/events/event_types.py
@@ -1,5 +1,5 @@
 class EventType(object):
     ACTIVATION = "activation"
     NODE_CHANGE = "node change"
-    GRAMMERS_LOADED = "grammars load"
+    GRAMMERS_LOADED = "grammars loaded"
     ON_ERROR = "on error"
